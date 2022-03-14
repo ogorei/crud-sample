3370012e6f8eefa1c13d5e939b471fbb6cc080bb
Reina Ogo <62832934+ogorei@users.noreply.github.com>
2022-03-14 23:03:04 +0900

    added icons

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {} from 'react-icons'
 
 type Props = {}
 
@@ -7,10 +8,23 @@ export default function TaskForm({}: Props) {
     <div className="card mb-4 card-body bg-secondary">
         <h1>Form</h1>
           <form action="">
-            <input 
-              type="text" 
+            <input
+              type="text"
               placeholder='write a task'
               className="form-control mb-6 rounded-0 border-1" />
+
+              <textarea
+                name="desc"
+                id="desc"
+                rows={5}
+                className="rounded-0 form-control"
+                placeholder='write a description'></textarea>
+
+                <button
+                  className="btn-primary border-1text-white"
+                >
+                  Save
+                </button>
           </form>
 
     </div>
